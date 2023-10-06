b5b9aa458417c99698d72acb767335853859932f
Jesse Jones <code@seqmanagement.com>
2023-10-06 07:05:49 -0700

    refactor table info from Enum to dict

--- xer_reader/src/table_data.py
+++ xer_reader/src/table_data.py
@@ -1,359 +1,357 @@
-from enum import Enum
-
-
-class TableInfo(Enum):
-    ACCOUNT = {
+table_data = {
+    "ACCOUNT": {
         "key": "acct_id",
         "description": "Cost Accounts",
         "depends": [],
-    }
-    ACTVCODE = {
+    },
+    "ACTVCODE": {
         "key": "actv_code_id",
         "description": "Activity Code Values",
         "depends": ["ACTVTYPE"],
-    }
-    ACTVTYPE = {
+    },
+    "ACTVTYPE": {
         "key": "actv_code_type_id",
         "description": "Activity Codes",
         "depends": [],
-    }
-    APPLYACTOPTIONS = {
+    },
+    "APPLYACTOPTIONS": {
         "key": None,
         "description": "Apply Actual Options",
         "depends": [],
-    }
-    ASGNMNTACAT = {
+    },
+    "ASGNMNTACAT": {
         "key": None,
         "description": "Assignment Code Assignments",
         "depends": ["ASGNMNTCATTYPE", "ASGNMNTCATVAL", "TASKRSRC"],
-    }
-    ASGNMNTCATTYPE = {
+    },
+    "ASGNMNTCATTYPE": {
         "key": "asgnmnt_catg_type_id",
         "description": "Assignment Codes",
         "depends": [],
-    }
-    ASGNMNTCATVAL = {
+    },
+    "ASGNMNTCATVAL": {
         "key": "asgnmnt_catg_id",
         "description": "Assignment Code Values",
         "depends": ["ASGNMNTCATTYPE"],
-    }
-    BUDGCHNG = {
+    },
+    "BUDGCHNG": {
         "key": "budg_chng_id",
         "description": "Budget Changes",
         "depends": [],
-    }
-    CALENDAR = {
+    },
+    "CALENDAR": {
         "key": "clndr_id",
         "description": "Calendars",
         "depends": [],
-    }
-    COSTTYPE = {
+    },
+    "COSTTYPE": {
         "key": "cost_type_id",
         "description": "Expense Categories",
         "depends": [],
-    }
-    CURRTYPE = {
+    },
+    "CURRTYPE": {
         "key": "curr_id",
         "description": "Currency Types",
         "depends": [],
-    }
-    DOCCATG = {
+    },
+    "DOCCATG": {
         "key": "doc_catg_id",
         "description": "Document Categories",
         "depends": [],
-    }
-    DOCSTAT = {
+    },
+    "DOCSTAT": {
         "key": "doc_status_id",
         "description": "Document Statuses",
         "depends": [],
-    }
-    DOCUMENT = {
+    },
+    "DOCUMENT": {
         "key": "doc_id",
         "description": "Work Products and Documents",
         "depends": [],
-    }
-    FINDATES = {
+    },
+    "FINDATES": {
         "key": "fin_dates_id",
         "description": "Financial Periods",
         "depends": [],
-    }
-    FUNDSRC = {
+    },
+    "FUNDSRC": {
         "key": "fund_id",
         "description": "Funding Sources",
         "depends": [],
-    }
-    ISSUHIST = {
+    },
+    "ISSUHIST": {
         "key": None,
         "description": "Notification History",
         "depends": [],
-    }
-    MEMOTYPE = {
+    },
+    "MEMOTYPE": {
         "key": "memo_type_id",
         "description": "Notebook Topics",
         "depends": [],
-    }
-    NONWORK = {
+    },
+    "NONWORK": {
         "key": "nonwork_type_id",
         "description": "Non-work Types",
         "depends": [],
-    }
-    OBS = {
+    },
+    "OBS": {
         "key": "obs_id",
         "description": "Organization Breakdown Structure",
         "depends": [],
-    }
-    PCATTYPE = {
+    },
+    "PCATTYPE": {
         "key": "proj_catg_type_id",
         "description": "Project Codes",
         "depends": [],
-    }
-    PCATVAL = {
+    },
+    "PCATVAL": {
         "key": "proj_catg_id",
         "description": "Project Code Values",
         "depends": ["PCATTYPE"],
-    }
-    PHASE = {
+    },
+    "PHASE": {
         "key": "phase_id",
         "description": "Phase Project Code",
         "depends": [],
-    }
-    POBS = {
+    },
+    "POBS": {
         "key": "pobs_id",
         "description": "Extra Data",
         "depends": [],
-    }
-    PROJCOST = {
+    },
+    "PROJCOST": {
         "key": "cost_item_id",
         "description": "Project Expenses",
         "depends": ["TASK"],
-    }
-    PROJECT = {
+    },
+    "PROJECT": {
         "key": "proj_id",
         "description": "Projects",
         "depends": [],
-    }
-    PROJEST = {
+    },
+    "PROJEST": {
         "key": "proj_est_id",
         "description": "Estimate History",
         "depends": [],
-    }
-    PROJFUND = {
+    },
+    "PROJFUND": {
         "key": "proj_fund_id",
         "description": "Project Funding Assignments",
         "depends": ["FUNDSRC"],
-    }
-    PROJISSU = {
+    },
+    "PROJISSU": {
         "key": "issue_id",
         "description": "Issues",
         "depends": [],
-    }
-    PROJPCAT = {
+    },
+    "PROJPCAT": {
         "key": None,
         "description": "Project Code Assignments",
         "depends": ["PCATVAL", "PCATTYPE"],
-    }
-    PROJTHRS = {
+    },
+    "PROJTHRS": {
         "key": "thresh_id",
         "description": "Thresholds",
         "depends": [],
-    }
-    PROJWBS = {
+    },
+    "PROJWBS": {
         "key": "wbs_id",
         "description": "Work Breakdown Structure",
         "depends": [],
-    }
-    PRORISK = {
+    },
+    "PRORISK": {
         "key": "risk_id",
         "description": "Risks",
         "depends": ["RISKTYPE"],
-    }
-    RCATTYPE = {
+    },
+    "RCATTYPE": {
         "key": "rsrc_catg_type_id",
         "description": "Resource Codes",
         "depends": [],
-    }
-    RCATVAL = {
+    },
+    "RCATVAL": {
         "key": "rsrc_catg_id",
         "description": "Resource Code Values",
         "depends": ["RCATTYPE"],
-    }
-    RISKTYPE = {
+    },
+    "RISKTYPE": {
         "key": "risk_type_id",
         "description": "Risk Types",
         "depends": [],
-    }
-    ROLECATTYPE = {
+    },
+    "ROLECATTYPE": {
         "key": "role_catg_type_id",
         "description": "Role Codes",
         "depends": [],
-    }
-    ROLECATVAL = {
+    },
+    "ROLECATVAL": {
         "key": "role_catg_id",
         "description": "Role Code Values",
         "depends": ["ROLECATTYPE"],
-    }
-    ROLELIMIT = {
+    },
+    "ROLELIMIT": {
         "key": "rolelimit_id",
         "description": "Role Limits",
         "depends": ["ROLES"],
-    }
-    ROLERATE = {
+    },
+    "ROLERATE": {
         "key": "role_rate_id",
         "description": "Role Prices",
         "depends": ["ROLES"],
-    }
-    ROLERCAT = {
+    },
+    "ROLERCAT": {
         "key": None,
         "description": "Role Code Assignments",
         "depends": ["ROLES", "ROLECATTYPE", "ROLECATVAL"],
-    }
-    ROLES = {
+    },
+    "ROLES": {
         "key": "role_id",
         "description": "Roles",
         "depends": [],
-    }
-    RSRC = {
+    },
+    "RSRC": {
         "key": "rsrc_id",
         "description": "Resources",
         "depends": [],
-    }
-    RSRCCURV = {
+    },
+    "RSRCCURV": {
         "key": "curv_id",
         "description": "Resource Curves",
         "depends": [],
-    }
-    RSRCCURVDATA = {
+    },
+    "RSRCCURVDATA": {
         "key": None,
         "description": "Resource Curve Data",
         "depends": ["RSRCCURV"],
-    }
-    RSRCLEVELLIST = {
+    },
+    "RSRCLEVELLIST": {
         "key": "rsrc_level_list_id",
         "description": "Resource Level List",
         "depends": ["RSRC"],
-    }
-    RSRCRATE = {
+    },
+    "RSRCRATE": {
         "key": "rsrc_rate_id",
         "description": "Resource Prices",
         "depends": ["RSRC"],
-    }
-    RSRCRCAT = {
+    },
+    "RSRCRCAT": {
         "key": None,
         "description": "Resource Code Assignments",
         "depends": ["RCATVAL", "RCATTYPE", "RSRC"],
-    }
-    RSRCROLE = {
+    },
+    "RSRCROLE": {
         "key": None,
         "description": "Resource Role Assignments",
         "depends": ["ROLES", "RSRC"],
-    }
-    SCHEDOPTIONS = {
+    },
+    "SCHEDOPTIONS": {
         "key": "schedoptions_id",
         "description": "Schedule Options",
         "depends": [],
-    }
-    SHIFT = {
+    },
+    "SHIFT": {
         "key": "shift_id",
         "description": "Shift Names",
         "depends": [],
-    }
-    SHIFTPER = {
+    },
+    "SHIFTPER": {
         "key": "shift_period_id",
         "description": "Shifts",
         "depends": ["SHIFT"],
-    }
-    TASK = {
+    },
+    "TASK": {
         "key": "task_id",
         "description": "Activities",
         "depends": ["CALENDAR"],
-    }
-    TASKACTV = {
+    },
+    "TASKACTV": {
         "key": None,
         "description": "Activity Code Assignments",
         "depends": ["ACTVCODE", "TASK"],
-    }
-    TASKDOC = {
+    },
+    "TASKDOC": {
         "key": "taskdoc_id",
         "description": "Document Assignments",
         "depends": ["DOCUMENT", "TASK"],
-    }
-    TASKFDBK = {
+    },
+    "TASKFDBK": {
         "key": None,
         "description": "Activity Feedback",
         "depends": ["TASK"],
-    }
-    TASKFIN = {
+    },
+    "TASKFIN": {
         "key": None,
         "description": "Activity Past Period Actuals",
         "depends": ["FINDATES", "TASK"],
-    }
-    TASKMEMO = {
+    },
+    "TASKMEMO": {
         "key": "memo_id",
         "description": "Activity Notebook",
         "depends": ["MEMOTYPE", "TASK"],
-    }
-    TASKNOTE = {
+    },
+    "TASKNOTE": {
         "key": None,
         "description": "Activity Notes to Resources",
         "depends": ["TASK"],
-    }
-    TASKPRED = {
+    },
+    "TASKPRED": {
         "key": "task_pred_id",
         "description": "Activity Relationships",
         "depends": ["TASK"],
-    }
-    TASKPROC = {
+    },
+    "TASKPROC": {
         "key": "proc_id",
         "description": "Activity Steps",
         "depends": ["TASK"],
-    }
-    TASKRSRC = {
+    },
+    "TASKRSRC": {
         "key": "taskrsrc_id",
         "description": "Activity Resource Assignments",
         "depends": ["RSRC", "TASK"],
-    }
-    TASKUSER = {
+    },
+    "TASKUSER": {
         "key": None,
         "description": "Activity Owners",
         "depends": ["TASK"],
-    }
-    THRSPARM = {
+    },
+    "THRSPARM": {
         "key": "thresh_parm_id",
         "description": "Threshold Parameters",
         "depends": [],
-    }
-    TRSRCFIN = {
+    },
+    "TRSRCFIN": {
         "key": None,
         "description": "Activity Resource Assignment Past Period Actuals",
         "depends": ["FINDATES", "TASKRSRC"],
-    }
-    UDFTYPE = {
+    },
+    "UDFTYPE": {
         "key": "udf_type_id",
         "description": "User Defined Fields",
         "depends": [],
-    }
-    UDFVALUE = {
+    },
+    "UDFVALUE": {
         "key": None,
         "description": "User Defined Field Values",
         "depends": ["UDFTYPE"],
-    }
-    UMEASURE = {
+    },
+    "UMEASURE": {
         "key": "unit_id",
         "description": "Units of Measure",
         "depends": [],
-    }
-    WBSBUDG = {
+    },
+    "WBSBUDG": {
         "key": "wbs_budg_id",
         "description": "Spending and Benefit Plans",
         "depends": [],
-    }
-    WBSMEMO = {
+    },
+    "WBSMEMO": {
         "key": "wbs_memo_id",
         "description": "EPS, Project and WBS Notebook",
         "depends": ["MEMOTYPE", "PROJWBS"],
-    }
-    WBSSTEP = {
+    },
+    "WBSSTEP": {
         "key": "wbs_step_id",
         "description": "WBS Milestones",
         "depends": [],
-    }
+    },
+}
